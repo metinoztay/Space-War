7369d35b971484c7697b0ffae49a5e604162366a
Metin ÖZTAY <metinoztay@gmail.com>
2023-01-31 23:54:35 +0300

    Prefabs

--- Space-War/Assets/Scripts/MovementController.cs
+++ Space-War/Assets/Scripts/MovementController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipController : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
